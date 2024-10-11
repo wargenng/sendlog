c49f468d7874cd4f0538247ceaa63a5b88c243ab
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-11 16:02:02 -0700

    basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/lXqKHYWPgVZHxCEneBV9a6jXxUs2gBWczRKwbpYEI3k5Dql1",
+  "https://utfs.io/f/lXqKHYWPgVZH4xd5vstvbmXysOqIkEZl9TnRht7jfcd42BKY",
+  "https://utfs.io/f/lXqKHYWPgVZHjU44gt9uIkxw0nlTbSiKChLzMcr8GV9JgF4X",
+  "https://utfs.io/f/lXqKHYWPgVZH3wQrY79CmsHi6MS2ecyPV08LWnvoN5ZRDTdq",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        TEST
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
       </div>
     </main>
   );
