4eb6064cba35aed07ee9d4f0d7574f9410255a8f
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-13 10:59:47 -0700

    adding delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { Button } from "~/components/ui/button";
 import { db } from "~/server/db";
-import { getMyImages } from "~/server/queries";
+import { deletePost, getMyImages } from "~/server/queries";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
@@ -17,6 +18,17 @@ export default async function HomePage() {
             <div key={post.id}>
               {post.name}
               <img src={post.url} className="w-48" />
+              <form
+                action={async () => {
+                  "use server";
+
+                  await deletePost(post.id);
+                }}
+              >
+                <Button type="submit" variant="destructive">
+                  delete
+                </Button>
+              </form>
             </div>
           ))}
         </div>
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,6 +1,9 @@
 import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { posts } from "./db/schema";
+import { revalidatePath } from "next/cache";
+import { eq, and } from "drizzle-orm";
 
 export async function getMyImages() {
   const user = auth();
@@ -13,4 +16,15 @@ export async function getMyImages() {
   });
 
   return posts;
+}
+
+export async function deletePost(id: number) {
+  const user = auth();
+  if (!user.userId) return [];
+
+  await db
+    .delete(posts)
+    .where(and(eq(posts.id, id), eq(posts.userId, user.userId)));
+
+  revalidatePath("/");
 }
