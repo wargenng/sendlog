f23bd96fe910b073b20ebce0c1ceb8f87344596d
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-14 14:33:45 -0700

    adding in climb fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getCurrentUsersClimbs } from "~/server/queries";
+import { locations } from "./utils/locations";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
@@ -11,14 +12,63 @@ export default async function HomePage() {
                 <div className="h-full w-full text-2xl">please sign in</div>
             </SignedOut>
             <SignedIn>
-                <div className="flex flex-col gap-4">
-                    {climbs.map((climb) => (
-                        <div key={climb.id}>
-                            <h1>
-                                {climb.name} {climb.grade}
-                            </h1>
-                        </div>
-                    ))}
+                <div className="space-y-2">
+                    <h1 className="border-b text-2xl font-semibold">recent</h1>
+                    <div className="flex flex-col gap-2">
+                        {climbs.map((climb) => (
+                            <div key={climb.id} className="border-b">
+                                <h1>
+                                    {climb.name} {climb.grade}
+                                </h1>
+                                {climb.rating ? (
+                                    <p className="flex items-center text-xl text-primary">
+                                        {"★".repeat(climb.rating)}
+                                        {"★"
+                                            .repeat(5 - climb.rating)
+                                            .split("")
+                                            .map((star, index) => (
+                                                <span
+                                                    key={index}
+                                                    className="text-gray-500"
+                                                >
+                                                    {star}
+                                                </span>
+                                            ))}{" "}
+                                        <span className="ml-2 text-sm text-gray-500">
+                                            ({climb.rating})
+                                        </span>
+                                    </p>
+                                ) : null}
+                                {climb.location ? (
+                                    <p>
+                                        {
+                                            locations.find(
+                                                (location) =>
+                                                    location.id ===
+                                                    climb.location,
+                                            )?.label
+                                        }
+                                    </p>
+                                ) : null}
+                                {climb.sendDate ? (
+                                    <p>
+                                        {climb.sendDate.toLocaleDateString(
+                                            undefined,
+                                            {
+                                                year: "numeric",
+                                                month: "long",
+                                                day: "numeric",
+                                            },
+                                        )}
+                                    </p>
+                                ) : null}
+                                {climb.attempts ? (
+                                    <p>{climb.attempts} attempts</p>
+                                ) : null}
+                                <p className="italic">{climb.notes}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </SignedIn>
         </main>
