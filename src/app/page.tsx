413f831fee909672001d22fa4f2e2d09ad38be6a
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-16 13:52:07 -0700

    loading was casuing some issue so removing temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { DataCarousel } from "./_components/datacarousel";
 import { RecentClimbs } from "./_components/recentclimbs";
 import { TopNav } from "./_components/topnav";
-import { Suspense } from "react";
-import Loading from "./loading";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
@@ -11,17 +9,15 @@ export default async function HomePage() {
         <main className="">
             <SignedIn>
                 <TopNav title="sendlog" />
-                <Suspense fallback={<Loading />}>
-                    <div className="mt-20 space-y-4 pb-32">
-                        <div className="space-y-2">
-                            <p className="px-6 text-2xl font-semibold">
-                                welcome back!
-                            </p>
-                            <DataCarousel />
-                        </div>
-                        <RecentClimbs />
+                <div className="mt-20 space-y-4 pb-32">
+                    <div className="space-y-2">
+                        <p className="px-6 text-2xl font-semibold">
+                            welcome back!
+                        </p>
+                        <DataCarousel />
                     </div>
-                </Suspense>
+                    <RecentClimbs />
+                </div>
             </SignedIn>
         </main>
     );
