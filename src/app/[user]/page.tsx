50bb102aa54f14bf3188a481bdb4d60a556d723f
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-16 18:24:40 -0700

    internal server error

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -2,12 +2,17 @@ import { clerkClient } from "@clerk/express";
 import { TopNav } from "../_components/topnav";
 
 export async function generateStaticParams() {
-    const response = await clerkClient.users.getUserList();
-    const users = response.data;
+    try {
+        const response = await clerkClient.users.getUserList();
+        const users = response.data;
 
-    return users.map((user) => ({
-        username: user.username,
-    }));
+        return users.map((user) => ({
+            username: user.username,
+        }));
+    } catch (error) {
+        console.error("Error generating static params:", error);
+        return [];
+    }
 }
 
 interface Params {
@@ -18,29 +23,35 @@ interface Params {
 
 export default async function UserPage({ params }: Params) {
     const { user: username } = params;
-    const response = await clerkClient.users.getUserList();
-    const users = response.data;
-    const user = users.find((user) => user.username === username);
 
-    if (!user) {
-        return <p>User not found</p>;
-    }
+    try {
+        const response = await clerkClient.users.getUserList();
+        const users = response.data;
+        const user = users.find((user) => user.username === username);
 
-    return (
-        <main>
-            <TopNav title={user.username ?? "404"} />
-            <div className="mt-20 px-6 pb-32">
-                <div className="flex gap-4">
-                    <img
-                        src={user?.imageUrl}
-                        className="h-24 w-24 rounded-full object-cover"
-                        alt="Profile"
-                    />
-                    <div>
-                        <h1 className="text-sm">{user.fullName}</h1>
+        if (!user) {
+            return <p>User not found</p>;
+        }
+
+        return (
+            <main>
+                <TopNav title={user.username ?? "404"} />
+                <div className="mt-20 px-6 pb-32">
+                    <div className="flex gap-4">
+                        <img
+                            src={user?.imageUrl}
+                            className="h-24 w-24 rounded-full object-cover"
+                            alt="Profile"
+                        />
+                        <div>
+                            <h1 className="text-sm">{user.fullName}</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
-    );
+            </main>
+        );
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+        return <p>Internal Server Error</p>;
+    }
 }
