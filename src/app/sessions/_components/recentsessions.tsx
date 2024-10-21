94b19f1dc4589c5ebd6ed1ad58188875e9515517
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-21 16:37:09 -0700

    refactoring recent sessions

---
+++ src/app/sessions/_components/recentsessions.tsx
@@ -0,0 +1,78 @@
+import { ChevronRight } from "lucide-react";
+import SessionDrawer from "~/common/sessiondrawer/sessiondrawer";
+import { getCurrentUsersSessions, SessionWithClimbs } from "~/server/queries";
+import SessionClimbs from "../../_components/sessionclimbs";
+import { locations } from "../../utils/locations";
+
+export async function RecentSessions() {
+    const sessions = (await getCurrentUsersSessions()) as SessionWithClimbs[];
+
+    return (
+        <div className="flex flex-col gap-2 rounded-lg bg-secondary/50 p-4">
+            {sessions.map((session) => (
+                <SessionDrawer
+                    climbs={SessionClimbs(session.id)}
+                    isEdit={true}
+                    session={session}
+                    key={session.id}
+                >
+                    <div
+                        className={`space-y-1 ${sessions.indexOf(session) === sessions.length - 1 ? "" : "border-b"}`}
+                    >
+                        <div className="flex items-center justify-between">
+                            <h1 className="text-xl">{session.name}</h1>
+                            <ChevronRight className="h-6 w-6 text-foreground/50" />
+                        </div>
+                        <p className="text-xs text-foreground/50">
+                            {locations[session.location - 1]?.label} ·{" "}
+                            {locations[session.location - 1]?.location},{" "}
+                            {locations[session.location - 1]?.state}
+                        </p>
+                        <p className="text-xs text-foreground/50">
+                            {new Date(session.date).toLocaleDateString("en-US")}
+                        </p>
+                        <div>
+                            <p className="flex flex-wrap text-xs font-light italic text-foreground/50">
+                                {session.notes}
+                            </p>
+                        </div>
+                        <div className="flex justify-between py-2 font-semibold">
+                            <div className="w-1/4">
+                                <div className="text-2xl">
+                                    {session.highestGrade}
+                                </div>{" "}
+                                <p className="text-xs font-light text-foreground/50">
+                                    Highest grade
+                                </p>
+                            </div>
+                            <div className="w-1/4">
+                                <div className="text-2xl">
+                                    {session.climbs.length}
+                                </div>{" "}
+                                <p className="text-xs font-light text-foreground/50">
+                                    Climbs sent
+                                </p>
+                            </div>
+                            <div className="w-1/4">
+                                <div className="text-2xl">
+                                    {session.totalAttempts}
+                                </div>{" "}
+                                <p className="text-xs font-light text-foreground/50">
+                                    Attempts
+                                </p>
+                            </div>
+                            <div className="w-1/4">
+                                <div className="text-2xl">
+                                    {session.totalVpoints}
+                                </div>{" "}
+                                <p className="text-xs font-light text-foreground/50">
+                                    V-points
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </SessionDrawer>
+            ))}
+        </div>
+    );
+}
--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -1,104 +1,17 @@
-import { ChevronRight } from "lucide-react";
-import SessionDrawer from "~/common/sessiondrawer/sessiondrawer";
-import { Button } from "~/components/ui/button";
-import { getCurrentUsersSessions, SessionWithClimbs } from "~/server/queries";
-import SessionClimbs from "../_components/sessionclimbs";
+import { Suspense } from "react";
 import { TopNav } from "../_components/topnav";
-import { locations } from "../utils/locations";
+import { RecentSessions } from "./_components/recentsessions";
 
 export default async function SessionsPage() {
-    const sessions = (await getCurrentUsersSessions()) as SessionWithClimbs[];
-
     return (
         <main className="">
             <TopNav title="sessions" />
             <div className="mt-20 space-y-4 px-6 pb-32">
-                <div>
-                    <SessionDrawer>
-                        <Button className="w-full text-foreground">
-                            + Create Session
-                        </Button>
-                    </SessionDrawer>
-                </div>
                 <div className="space-y-2">
-                    <h1 className="border-b text-base font-bold">
-                        recent sessions
-                    </h1>
-                    <div className="flex flex-col gap-2 rounded-lg bg-secondary/50 p-4">
-                        {sessions.map((session) => (
-                            <SessionDrawer
-                                climbs={SessionClimbs(session.id)}
-                                isEdit={true}
-                                session={session}
-                                key={session.id}
-                            >
-                                <div
-                                    className={`space-y-1 ${sessions.indexOf(session) === sessions.length - 1 ? "" : "border-b"}`}
-                                >
-                                    <div className="flex items-center justify-between">
-                                        <h1 className="text-xl">
-                                            {session.name}
-                                        </h1>
-                                        <ChevronRight className="h-6 w-6 text-foreground/50" />
-                                    </div>
-                                    <p className="text-xs text-foreground/50">
-                                        {locations[session.location - 1]?.label}{" "}
-                                        ·{" "}
-                                        {
-                                            locations[session.location - 1]
-                                                ?.location
-                                        }
-                                        ,{" "}
-                                        {locations[session.location - 1]?.state}
-                                    </p>
-                                    <p className="text-xs text-foreground/50">
-                                        {new Date(
-                                            session.date,
-                                        ).toLocaleDateString("en-US")}
-                                    </p>
-                                    <div>
-                                        <p className="flex flex-wrap text-xs font-light italic text-foreground/50">
-                                            {session.notes}
-                                        </p>
-                                    </div>
-                                    <div className="flex justify-between py-2 font-semibold">
-                                        <div className="w-1/4">
-                                            <div className="text-2xl">
-                                                {session.highestGrade}
-                                            </div>{" "}
-                                            <p className="text-xs font-light text-foreground/50">
-                                                Highest grade
-                                            </p>
-                                        </div>
-                                        <div className="w-1/4">
-                                            <div className="text-2xl">
-                                                {session.climbs.length}
-                                            </div>{" "}
-                                            <p className="text-xs font-light text-foreground/50">
-                                                Climbs sent
-                                            </p>
-                                        </div>
-                                        <div className="w-1/4">
-                                            <div className="text-2xl">
-                                                {session.totalAttempts}
-                                            </div>{" "}
-                                            <p className="text-xs font-light text-foreground/50">
-                                                Attempts
-                                            </p>
-                                        </div>
-                                        <div className="w-1/4">
-                                            <div className="text-2xl">
-                                                {session.totalVpoints}
-                                            </div>{" "}
-                                            <p className="text-xs font-light text-foreground/50">
-                                                V-points
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </SessionDrawer>
-                        ))}
-                    </div>
+                    <h1 className="text-base font-bold">recent sessions</h1>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <RecentSessions />
+                    </Suspense>
                 </div>
             </div>
         </main>
