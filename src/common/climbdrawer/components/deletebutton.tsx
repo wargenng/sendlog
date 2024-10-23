c58d6553a0418c1d7f0c8a036aff50126f2f9dba
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-10-22 17:14:42 -0700

    adding delete dialog box

--- src/common/climbdrawer/components/deletebutton.tsx
+++ src/common/climbdrawer/components/deletebutton.tsx
@@ -3,6 +3,16 @@ import { useState } from "react";
 import { deleteClimb } from "~/app/api/climbActions";
 import { LoadingAnimation } from "~/components/loadinganimation";
 import { Button } from "~/components/ui/button";
+import { useToast } from "~/hooks/use-toast";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "~/components/ui/dialog";
 
 interface DeleteButtonProps {
     setIsUploading: React.Dispatch<React.SetStateAction<boolean>>;
@@ -19,26 +29,47 @@ export function DeleteClimbForm({
 }: DeleteButtonProps) {
     const [isDeleting, setIsDeleting] = useState(false);
     const router = useRouter();
+    const { toast } = useToast();
 
     return (
-        <form
-            onSubmit={async (e) => {
-                e.preventDefault();
-                setIsDeleting(true);
-                setIsUploading(true);
-                console.log("deleting form");
-                await deleteClimb(climbId);
-                console.log("deleting form");
-                router.refresh();
-                setIsDeleting(false);
-                setIsUploading(false);
-                setOpen(false);
-                setIsRejected(false);
-            }}
-        >
-            <Button variant="destructive" className="w-full">
-                {isDeleting ? <LoadingAnimation /> : "Delete Climb"}
-            </Button>
-        </form>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="destructive" className="w-full">
+                    {isDeleting ? <LoadingAnimation /> : "Delete Climb"}
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your climb.
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <form
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            setIsDeleting(true);
+                            setIsUploading(true);
+                            console.log("deleting form");
+                            await deleteClimb(climbId);
+                            console.log("deleting form");
+                            toast({
+                                title: "Deleted Climb",
+                                description: "Your climb has been deleted.",
+                            });
+                            router.refresh();
+                            setIsDeleting(false);
+                            setIsUploading(false);
+                            setOpen(false);
+                            setIsRejected(false);
+                        }}
+                    >
+                        <Button type="submit">Delete Climb</Button>
+                    </form>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 }
