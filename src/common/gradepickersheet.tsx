8556993925686a574d462386b3420c6595f734b5
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-11-16 20:15:37 -0800

    added done button

--- src/common/gradepickersheet.tsx
+++ src/common/gradepickersheet.tsx
@@ -9,6 +9,7 @@ import { Button } from "~/components/ui/button";
 import { Label } from "~/components/ui/label";
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetFooter,
@@ -47,79 +48,88 @@ export function GradePickerSheet({
             <SheetTrigger className="w-full" asChild>
                 {children}
             </SheetTrigger>
-            <SheetContent className="flex w-full flex-col justify-center">
+            <SheetContent className="flex w-full flex-col">
                 <SheetHeader>
                     <SheetTitle>Enter Climbs</SheetTitle>
                     <SheetDescription></SheetDescription>
                 </SheetHeader>
-                <div>
-                    <h1>added grades</h1>
-                    <div className="h-8">{bulk.join(", ")}</div>
-                </div>
-                <div className="flex h-full w-full flex-col items-center justify-center">
-                    <div className="flex flex-col items-center justify-center space-y-2">
-                        <Label>Current Grade</Label>
-                        <h1 className="text-8xl font-bold">{fullGrade}</h1>
+                <div className="flex h-full flex-col justify-between">
+                    <div>
+                        <h1>added grades</h1>
+                        <div className="h-8">{bulk.join(", ")}</div>
+                    </div>
+                    <div className="flex w-full flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center space-y-2">
+                            <Label>Current Grade</Label>
+                            <h1 className="text-8xl font-bold">{fullGrade}</h1>
+                        </div>
+                    </div>
+                    <div className="flex w-full justify-between">
+                        <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
+                            <Label>Grade</Label>
+                            <Button
+                                className="w-full"
+                                onClick={() => {
+                                    setGrade(grade < grades ? grade + 1 : 0);
+                                }}
+                            >
+                                <ArrowBigUp className="h-6 w-6" />
+                            </Button>
+                            <Button
+                                className="w-full"
+                                onClick={() => {
+                                    setGrade(grade > 0 ? grade - 1 : grades);
+                                }}
+                            >
+                                <ArrowBigDown className="h-6 w-6" />
+                            </Button>
+                        </div>
+                        <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
+                            <Button
+                                className="h-20 w-20 rounded-full bg-accent-2 text-foreground"
+                                variant="none"
+                                onClick={() => {
+                                    setBulk([...bulk, fullGrade]);
+                                }}
+                            >
+                                <ArrowUp className="h-12 w-12" />
+                            </Button>
+                        </div>
+                        <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
+                            <Label>Modifier</Label>
+                            <Button
+                                className="w-full"
+                                onClick={() => {
+                                    setModifier(
+                                        modifier < modifiers.length - 1
+                                            ? modifier + 1
+                                            : 0,
+                                    );
+                                }}
+                            >
+                                <ArrowBigUp className="h-6 w-6" />
+                            </Button>
+                            <Button
+                                className="w-full"
+                                onClick={() => {
+                                    setModifier(
+                                        modifier > 0
+                                            ? modifier - 1
+                                            : modifiers.length - 1,
+                                    );
+                                }}
+                            >
+                                <ArrowBigDown className="h-6 w-6" />
+                            </Button>
+                        </div>
                     </div>
                 </div>
                 <SheetFooter className="flex w-full flex-row items-end justify-center gap-6 pb-6">
-                    <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
-                        <Label>Grade</Label>
-                        <Button
-                            className="w-full"
-                            onClick={() => {
-                                setGrade(grade < grades ? grade + 1 : 0);
-                            }}
-                        >
-                            <ArrowBigUp className="h-6 w-6" />
+                    <SheetClose className="w-full">
+                        <Button className="bg-accent-3 w-full text-accent-foreground">
+                            Done
                         </Button>
-                        <Button
-                            className="w-full"
-                            onClick={() => {
-                                setGrade(grade > 0 ? grade - 1 : grades);
-                            }}
-                        >
-                            <ArrowBigDown className="h-6 w-6" />
-                        </Button>
-                    </div>
-                    <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
-                        <Button
-                            className="h-20 w-20 rounded-full bg-accent-2 text-foreground"
-                            variant="none"
-                            onClick={() => {
-                                setBulk([...bulk, fullGrade]);
-                            }}
-                        >
-                            <ArrowUp className="h-12 w-12" />
-                        </Button>
-                    </div>
-                    <div className="flex w-1/5 flex-col items-center justify-center space-y-1">
-                        <Label>Modifier</Label>
-                        <Button
-                            className="w-full"
-                            onClick={() => {
-                                setModifier(
-                                    modifier < modifiers.length - 1
-                                        ? modifier + 1
-                                        : 0,
-                                );
-                            }}
-                        >
-                            <ArrowBigUp className="h-6 w-6" />
-                        </Button>
-                        <Button
-                            className="w-full"
-                            onClick={() => {
-                                setModifier(
-                                    modifier > 0
-                                        ? modifier - 1
-                                        : modifiers.length - 1,
-                                );
-                            }}
-                        >
-                            <ArrowBigDown className="h-6 w-6" />
-                        </Button>
-                    </div>
+                    </SheetClose>
                 </SheetFooter>
             </SheetContent>
         </Sheet>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,6 +41,7 @@ export default {
                     DEFAULT: "hsl(var(--accent))",
                     foreground: "hsl(var(--accent-foreground))",
                     "2": "hsl(var(--accent-2))",
+                    "3": "hsl(var(--accent-3))",
                 },
                 destructive: {
                     DEFAULT: "hsl(var(--destructive))",
