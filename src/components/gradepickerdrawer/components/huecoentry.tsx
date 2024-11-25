bbb59c4b6d932fb1c848e76b36398d0d13aff961
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-11-25 12:04:15 -0800

    adjusting styles on entry

--- src/components/gradepickerdrawer/components/huecoentry.tsx
+++ src/components/gradepickerdrawer/components/huecoentry.tsx
@@ -1,4 +1,5 @@
-import { Ban, ChevronDown, ChevronUp } from "lucide-react";
+import { get } from "http";
+import { Ban, ChevronDown, ChevronUp, X } from "lucide-react";
 import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import type { Climb } from "~/server/db/schema";
@@ -9,15 +10,18 @@ interface HuecoEntryProps {
 }
 const modifiers = ["-", "+", "/", ""];
 const getFullGrade = (grade: number, modifier: number) => {
-    return `V${grade}${
-        modifiers[modifier] === "/" ? `/${grade + 1}` : `${modifiers[modifier]}`
-    }`;
+    return `V${grade}${getModifier(grade, modifier)}`;
+};
+const getModifier = (grade: number, modifier: number) => {
+    return modifiers[modifier] === "/"
+        ? `/${grade + 1}`
+        : `${modifiers[modifier]}`;
 };
 
 export function HuecoEntry({ hueco, setHueco }: HuecoEntryProps) {
     const grades = 17;
     const [grade, setGrade] = useState(0);
-    const [modifier, setModifier] = useState(1);
+    const [modifier, setModifier] = useState(3);
     const handleGradeChange = (grade: number) => {
         setGrade(grade);
         setHueco({
@@ -27,13 +31,54 @@ export function HuecoEntry({ hueco, setHueco }: HuecoEntryProps) {
     };
 
     return (
-        <div className="flex w-full flex-col items-center justify-center space-y-4 pb-12">
+        <div className="flex w-full flex-col items-center justify-center space-y-6 pb-12">
+            <div className="flex w-full items-center justify-between">
+                <Button
+                    className="flex h-12 w-12 items-center rounded-full border p-0"
+                    onClick={() => {
+                        handleGradeChange(grade > 0 ? grade - 1 : grades);
+                    }}
+                    variant="none"
+                >
+                    <ChevronDown className="h-6 w-6" />
+                </Button>
+                <div className="flex h-max items-start justify-between gap-1 font-bold">
+                    <p
+                        className={`text-center opacity-0 ${modifier < 2 ? "text-4xl" : "text-2xl"}`}
+                    >
+                        {getModifier(grade, modifier)}
+                    </p>
+                    <Button
+                        variant="none"
+                        className="flex h-max p-0 font-bold"
+                        onClick={() => {
+                            handleGradeChange(grade < grades ? grade + 1 : 0);
+                        }}
+                    >
+                        <h1 className="text-8xl">{"V" + grade}</h1>
+                    </Button>
+                    <p
+                        className={`text-center opacity-50 ${modifier < 2 ? "text-4xl" : "text-2xl"}`}
+                    >
+                        {getModifier(grade, modifier)}
+                    </p>
+                </div>
+                <Button
+                    className="flex h-12 w-12 items-center rounded-full border p-0"
+                    variant="none"
+                    onClick={() => {
+                        handleGradeChange(grade < grades ? grade + 1 : 0);
+                    }}
+                >
+                    <ChevronUp className="h-6 w-6" />
+                </Button>
+            </div>
             <div className="flex items-center justify-center gap-4">
                 {modifiers.map((mod, i) => (
                     <Button
                         key={mod + i}
                         variant="none"
-                        className={`flex h-12 w-12 space-x-1 rounded-full border p-2 text-foreground ${modifier === i ? "bg-primary text-primary-foreground" : ""}`}
+                        className={`${i !== 3 ? "h-12 w-12 rounded-full border p-2 text-foreground" : "p-0"} ${modifier === i && modifier !== 3 ? "bg-primary text-primary-foreground" : ""}`}
                         onClick={() => {
                             const mod = modifier === i ? 3 : i;
                             setModifier(mod);
@@ -44,41 +89,10 @@ export function HuecoEntry({ hueco, setHueco }: HuecoEntryProps) {
                         }}
                     >
                         <p className="text-2xl">
-                            {mod ? mod : <Ban size={20} />}
+                            {mod ? mod : <X size={20} />}
                         </p>
                     </Button>
                 ))}
-            </div>
-            <div className="flex w-full items-center justify-center">
-                <Button
-                    variant="none"
-                    className="h-max text-8xl font-bold"
-                    onClick={() => {
-                        handleGradeChange(grade < grades ? grade + 1 : 0);
-                    }}
-                >
-                    {getFullGrade(grade, modifier)}
-                </Button>
-            </div>
-            <div className="flex w-full items-center justify-center gap-4">
-                <Button
-                    className="flex h-12 w-12 items-center rounded-full border p-0"
-                    variant="none"
-                    onClick={() => {
-                        handleGradeChange(grade < grades ? grade + 1 : 0);
-                    }}
-                >
-                    <ChevronUp className="h-6 w-6" />
-                </Button>
-                <Button
-                    className="flex h-12 w-12 items-center rounded-full border p-0"
-                    onClick={() => {
-                        handleGradeChange(grade > 0 ? grade - 1 : grades);
-                    }}
-                    variant="none"
-                >
-                    <ChevronDown className="h-6 w-6" />
-                </Button>
             </div>
         </div>
     );
